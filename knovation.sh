ceff06067da309080cefd32cbf7206ffa49f10bd
Adam Blum <adam@opened.io>
2018-02-26 09:01:41 -0700

    key and secret and token and REST endpoints
    key and secret will be deleted later

--- knovation.sh
+++ knovation.sh
@@ -1,1 +1,2 @@
+# will be deleted later
 ruby srch_cli.rb -i 11948993788165717UO_4326 -k c61586d249852666735bcacd09bd0617e223413a7d0e6857638e8e96db10fcbb -t https://auth.sandbox.knov8.services/oauth/token?grant_type=client_credentials -b https://school.sandbox.nettrekker.com -s $1
