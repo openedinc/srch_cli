4385ad216690f8b818a2c020b5ef92b2b1cd3ded
Adam Blum <adam@openedinstitute.org>
2018-04-16 13:49:46 -0700

    LTI resource search for learning objectives now works in three different ways by URI, GUID or name

--- opened.sh
+++ opened.sh
@@ -1,1 +1,1 @@
-ruby srch_cli.rb -i cc18d57bc1ab578fc6003b5feaff5875e86648a59b5341122e5761b45a3e2257 -k 15ace16509692805e280c3d8eda0351d6a323ab9cbd17d1b1239026fb9e622ce  -b "https://resource-api-staging.herokuapp.com" -t "https://partner-staging.opened.com/2/oauth/get_token" -u bluma@act.org -s $1 -o $2
+ruby srch_cli.rb -i cc18d57bc1ab578fc6003b5feaff5875e86648a59b5341122e5761b45a3e2257 -k 15ace16509692805e280c3d8eda0351d6a323ab9cbd17d1b1239026fb9e622ce  -b "https://resource-api-staging.herokuapp.com" -t "https://partner-staging.opened.com/2/oauth/get_token" -u bluma@act.org -o $1
